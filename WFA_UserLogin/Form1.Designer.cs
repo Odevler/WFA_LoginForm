ed45632a271cfe7cea21c639dfea93d5173e3bda
muratvuranok <mrtvrnk@gmail.com>
2019-11-09 12:06:14 +0300

    Add project files.

---
+++ WFA_UserLogin/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WFA_UserLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.lblRastgeleSayi = new MetroFramework.Controls.MetroLabel();
+            this.txtGirisAlani = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.star4 = new System.Windows.Forms.PictureBox();
+            this.star5 = new System.Windows.Forms.PictureBox();
+            this.star3 = new System.Windows.Forms.PictureBox();
+            this.star2 = new System.Windows.Forms.PictureBox();
+            this.star1 = new System.Windows.Forms.PictureBox();
+            this.btnLogin = new MetroFramework.Controls.MetroTile();
+            ((System.ComponentModel.ISupportInitialize)(this.star4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.Location = new System.Drawing.Point(23, 60);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(303, 30);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Lütfen Sayıları TextBox\'a Giriniz";
+            // 
+            // lblRastgeleSayi
+            // 
+            this.lblRastgeleSayi.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblRastgeleSayi.Location = new System.Drawing.Point(23, 99);
+            this.lblRastgeleSayi.Name = "lblRastgeleSayi";
+            this.lblRastgeleSayi.Size = new System.Drawing.Size(303, 30);
+            this.lblRastgeleSayi.TabIndex = 1;
+            this.lblRastgeleSayi.Text = "* * * * * * * *";
+            // 
+            // txtGirisAlani
+            // 
+            // 
+            // 
+            // 
+            this.txtGirisAlani.CustomButton.Image = null;
+            this.txtGirisAlani.CustomButton.Location = new System.Drawing.Point(488, 2);
+            this.txtGirisAlani.CustomButton.Name = "";
+            this.txtGirisAlani.CustomButton.Size = new System.Drawing.Size(31, 31);
+            this.txtGirisAlani.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.txtGirisAlani.CustomButton.TabIndex = 1;
+            this.txtGirisAlani.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.txtGirisAlani.CustomButton.UseSelectable = true;
+            this.txtGirisAlani.CustomButton.Visible = false;
+            this.txtGirisAlani.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.txtGirisAlani.Lines = new string[0];
+            this.txtGirisAlani.Location = new System.Drawing.Point(23, 132);
+            this.txtGirisAlani.MaxLength = 32767;
+            this.txtGirisAlani.Name = "txtGirisAlani";
+            this.txtGirisAlani.PasswordChar = '\0';
+            this.txtGirisAlani.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.txtGirisAlani.SelectedText = "";
+            this.txtGirisAlani.SelectionLength = 0;
+            this.txtGirisAlani.SelectionStart = 0;
+            this.txtGirisAlani.ShortcutsEnabled = true;
+            this.txtGirisAlani.Size = new System.Drawing.Size(522, 36);
+            this.txtGirisAlani.TabIndex = 2;
+            this.txtGirisAlani.UseSelectable = true;
+            this.txtGirisAlani.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.txtGirisAlani.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel2.Location = new System.Drawing.Point(23, 260);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(143, 30);
+            this.metroLabel2.TabIndex = 6;
+            this.metroLabel2.Text = "Kalan Hakkınız : ";
+            // 
+            // star4
+            // 
+            this.star4.Image = global::WFA_UserLogin.Properties.Resources.icons8_star_30px_3;
+            this.star4.Location = new System.Drawing.Point(298, 260);
+            this.star4.Name = "star4";
+            this.star4.Size = new System.Drawing.Size(30, 30);
+            this.star4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star4.TabIndex = 5;
+            this.star4.TabStop = false;
+            // 
+            // star5
+            // 
+            this.star5.Image = global::WFA_UserLogin.Properties.Resources.icons8_star_30px_3;
+            this.star5.Location = new System.Drawing.Point(334, 260);
+            this.star5.Name = "star5";
+            this.star5.Size = new System.Drawing.Size(30, 30);
+            this.star5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star5.TabIndex = 5;
+            this.star5.TabStop = false;
+            // 
+            // star3
+            // 
+            this.star3.Image = global::WFA_UserLogin.Properties.Resources.icons8_star_30px_3;
+            this.star3.Location = new System.Drawing.Point(262, 260);
+            this.star3.Name = "star3";
+            this.star3.Size = new System.Drawing.Size(30, 30);
+            this.star3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star3.TabIndex = 4;
+            this.star3.TabStop = false;
+            // 
+            // star2
+            // 
+            this.star2.Image = global::WFA_UserLogin.Properties.Resources.icons8_star_30px_3;
+            this.star2.Location = new System.Drawing.Point(226, 260);
+            this.star2.Name = "star2";
+            this.star2.Size = new System.Drawing.Size(30, 30);
+            this.star2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star2.TabIndex = 5;
+            this.star2.TabStop = false;
+            // 
+            // star1
+            // 
+            this.star1.Image = global::WFA_UserLogin.Properties.Resources.icons8_star_30px_3;
+            this.star1.Location = new System.Drawing.Point(190, 260);
+            this.star1.Name = "star1";
+            this.star1.Size = new System.Drawing.Size(30, 30);
+            this.star1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.star1.TabIndex = 4;
+            this.star1.TabStop = false;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.ActiveControl = null;
+            this.btnLogin.Location = new System.Drawing.Point(23, 174);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(522, 66);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.TileImage = global::WFA_UserLogin.Properties.Resources.icons8_approved_unlock_50px;
+            this.btnLogin.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnLogin.TileTextFontSize = MetroFramework.MetroTileTextSize.Tall;
+            this.btnLogin.UseSelectable = true;
+            this.btnLogin.UseTileImage = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 303);
+            this.Controls.Add(this.metroLabel2);
+            this.Controls.Add(this.star4);
+            this.Controls.Add(this.star5);
+            this.Controls.Add(this.star3);
+            this.Controls.Add(this.star2);
+            this.Controls.Add(this.star1);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtGirisAlani);
+            this.Controls.Add(this.lblRastgeleSayi);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "Form1";
+            this.Text = "Kullanıcı Giriş Ekranı";
+            ((System.ComponentModel.ISupportInitialize)(this.star4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.star1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel lblRastgeleSayi;
+        private MetroFramework.Controls.MetroTextBox txtGirisAlani;
+        private MetroFramework.Controls.MetroTile btnLogin;
+        private System.Windows.Forms.PictureBox star1;
+        private System.Windows.Forms.PictureBox star2;
+        private System.Windows.Forms.PictureBox star3;
+        private System.Windows.Forms.PictureBox star5;
+        private System.Windows.Forms.PictureBox star4;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+    }
+}
+
